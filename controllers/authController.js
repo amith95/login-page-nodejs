6a38e17d420a57133d135062bf481e97166e44b5
amith <amithmp2012@gmail.com>
2024-03-31 09:54:19 +0530

    shirts added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,7 @@
 const USER = [
   {
-    username: "syamlal",
-    password: "password2",
-  },
-  {
     username: "amith",
     password: "password",
-  },
-  {
-    username: "manilal",
-    password: "password3",
   },
 ];
 
@@ -27,7 +19,7 @@ exports.post_login = async (req, res) => {
       req.session.user = user;
       res.json({ username });
     } else {
-      res.status(401).json({ error: "No user exist" });
+      res.status(401).json({ userList:USER,error: "No user exist" });
     }
   } catch (error) {
     console.log("user is not defined ", error);
---
+++ productController.js
@@ -0,0 +1,34 @@
+const shirts=[  {
+    Image:"images/img_1.webp",
+    name:"shirt1",
+    price:700
+},
+{
+    Image:"images/img_2.webp",
+    name:"shirt2",
+    price:700
+},
+{
+    Image:"images/img_3.webp",
+    name:"shirt3",
+    price:700
+},
+{
+    Image:"images/img_4.webp",
+    name:"shirt4",
+    price:700
+},
+{
+    Image:"images/img_5.webp",
+    name:"shirt5",
+    price:700
+},
+{
+    Image:"images/img_6.webp",
+    name:"shirt6",
+    price:700
+},
+
+
+];
+module.exports =shirts
